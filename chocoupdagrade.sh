96209f5d44644a26d9d70f175da37cb989a018bc
Chocolate Explosion <18145983+ch0colate-3xplosion@users.noreply.github.com>
2022-08-30 00:21:33 -0600

    Update chocoupdagrade.sh

--- chocoupdagrade.sh
+++ chocoupdagrade.sh
@@ -6,9 +6,28 @@ sudo apt update -y &> /dev/null
 echo "Now Upgrading Instance"
 sudo apt upgrade -y & /dev/null
 
+echo "Creating new Bash script to run on root folder as root"
+touch /root/linuxautoupdaupgrademe.sh
+cat > /root/linuxautoupdaupgrademe.sh << EOF
+echo "Now Updating Instance"
+sudo apt update -y &> /dev/null
+
+echo "Now Upgrading Instance"
+sudo apt upgrade -y & /dev/null
+
+echo "Rebooting Now..."
+sudo reboot
+EOF
+
+echo "Changing Bash Script ownership to Root"
+chown -R root /root/linuxautoupdaupgrademe.sh
+
+echo "Changing Bash Script Permissions"
+chmod 700 /root/linuxautoupdaupgrademe.sh
+
 #Minutes-Hour-Day-Month-Week At 00:00 on 1st Day of the Month the script will run 
 echo "Adding this Bash script to cronjob"
-(crontab -l 2>/dev/null ; echo "0 0 1 * * /usr/bin/php /var/www/$moodleInstanceName/admin/cli/cron.php" ) | crontab -
+(crontab -l 2>/dev/null ; echo "0 0 1 * * /root/linuxautoupdaupgrademe.sh" ) | crontab -
 
-echo "Rebooting . . ."
+echo "Rebooting Now..."
 sudo reboot
