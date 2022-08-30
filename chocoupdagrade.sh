007036064c488d86ee8827a65323e83897b75813
Chocolate Explosion <18145983+ch0colate-3xplosion@users.noreply.github.com>
2022-08-30 00:14:03 -0600

    Update chocoupdagrade.sh

--- chocoupdagrade.sh
+++ chocoupdagrade.sh
@@ -5,3 +5,10 @@ sudo apt update -y &> /dev/null
 
 echo "Now Upgrading Instance"
 sudo apt upgrade -y & /dev/null
+
+#Minutes-Hour-Day-Month-Week At 00:00 on 1st Day of the Month the script will run 
+echo "Adding this Bash script to cronjob"
+(crontab -l 2>/dev/null ; echo "0 0 1 * * /usr/bin/php /var/www/$moodleInstanceName/admin/cli/cron.php" ) | crontab -
+
+echo "Rebooting . . ."
+sudo reboot
